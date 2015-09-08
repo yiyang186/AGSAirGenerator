e1aec3e72f27aa7725c43523a828fadcf6252304
yiyang186 <454750333@qq.com>
2015-09-08 16:36:38 +0800

    correct actail

--- lib/convertor.py
+++ lib/convertor.py
@@ -14,9 +14,7 @@ def convertfile(filename, output, recoders, today):
 			actail = str(booksheet.cell(row, 0).value)[0:6]
 			if not actail: continue
 			if '.' in actail: actail = 'B-' + actail[0:4]
-			if '-' not in actail: actail = 'B-' + actail[1:4]
-			if len(actail) != 6:
-				print actail
+			if '-' not in actail: actail = 'B-' + actail[1:5]
 			if filedata.has_key(actail):
 				filedata[actail]['frame'] = str(booksheet.cell(row, 6).value)
 				continue
@@ -39,10 +37,12 @@ def convertfile(filename, output, recoders, today):
 		airrow[2] = rowdict['airline']
 		airrow[3] = rowdict['type']
 		airrow[7] = 1
-		airrow[8] = recoders['qar'].get(rowdict.get('qar'))
+		qarinfo = recoders['qar'].get(rowdict.get('qar'))
+		if qarinfo:
+			airrow[8] = int(qarinfo) - 1 	# maint recoder的索引从0开始
 		airrow[9] = recoders['qar'].get(rowdict.get('dar'))
 		fdrinfo = recoders['fdr'].get(rowdict.get('fdr'))
 		if fdrinfo:
-			airrow[10] = '10' + fdrinfo		# crash recoder的索引从1001开始
+			airrow[10] = 1000 + int(fdrinfo)		# crash recoder的索引从1001开始
 		airrow[32] = rowdict['engine']		# Engine 1 type
 		output.writerow(airrow)
